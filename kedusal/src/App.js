e36da972cc445d09e04f01e01ec2e453f1e4ba45
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-04-12 00:24:48 +0200

    Update api

--- kedusal/src/App.js
+++ kedusal/src/App.js
@@ -10,30 +10,60 @@ const sampleId = {
   height: 1.5,
   eyeColor: 'blue',
   species: "human",
-  affilations: ["Confederacy of Independent Systems ", "Separatist Droid Army"],
   skinColor: "black",
-  name: 'kedusal'
+  name: 'kedusal',
+  image:""
 
 }
 
 class App extends Component {
   state = {
-    charac: sampleId
+    charac: sampleId,
+    userChoice: ''
   }
 
-  getLove() {
+  getLovpoulet() {
     const proxis = "https://cors-anywhere.herokuapp.com/"
     fetch(proxis + "https://melroune.github.io/starwars-api/api/all.json")
       .then(response => response.json())
       .then(data => {
-        this.setState({charac: data[0]})
+         const random = Math.floor(Math.random() * data.length)
+        this.setState({charac :data[random]})
       })
   }
+
+  handleUserChoice = choice => {
+    this.setState({
+      userChoice: choice
+    })
+  }
+
+  handleMyChoice = () => {
+    if (this.state.userChoice === 'poulet') {
+      return (
+        < h1 > Moi j'aime le poulet </h1>
+      )
+    } else if (this.state.userChoice === 'yolo') {
+      return ( <h1 > Moi je suis yolo </h1>
+      )
+    } else return ( <h1> Trou Noir </h1>
+    )
+  }
+
   render() {
     return (
       <div>
         <DisplayApi charac={this.state.charac} />
         <GenerateId selectId={() => this.getLove()} />
+         < button onClick = {
+           () => this.handleUserChoice('poulet')
+         } > poulet </button>
+         < button onClick = {
+           () => this.handleUserChoice('yolo')
+         } > yolo </button>
+
+         {this.handleMyChoice()}
+
       </div>
     )
   }
