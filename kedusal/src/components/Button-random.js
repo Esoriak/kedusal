4842ad1f1dc0dfdbacc0465292f692b73665c106
Esoriak <somahaut.dev@gmail.com>
2019-04-11 16:26:18 +0200

    add button random

---
+++ kedusal/src/components/Button-random.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render() {
+      return (
+            <div className="button-random">
+            
+            <button onClick={} > J'ai de la force </button>
+            
+            </div>
+      );
+    }
+}
+
+
+
+export default Button 
